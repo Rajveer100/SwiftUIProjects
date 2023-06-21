7f9b2dd0525d16d3be4cc08c62fcca20c754250b
Rajveer <rajveer.developer@icloud.com>
2023-06-21 12:52:54 +0530

    Fix State of Apps

---
+++ Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,38 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Rajveer Singh on 20/06/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .navigationTitle("Featured")
+                .listRowInsets(EdgeInsets())
+            }
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
